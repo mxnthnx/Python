8a0d606ee5813c203ad791038b9f4a60d205f2b3
MXNTHN <barotmanthan1748@gmail.com>
2026-08-19 17:25:31 +0530

    Basics.

---
+++ codes/basic.py
@@ -0,0 +1,32 @@
+#Using 'print' on the same line with the help of semicolon(;).
+
+print("Kill yourself"); print("Kill yourself"); print("Fucking Kill yourself")
+
+#Using end parameter to print on the same line.
+
+print("How about", end=" ")
+print("killing yourself?")
+
+#Printing numbers
+
+print(5)
+print(69)
+print(666)
+
+#Math.
+
+print(6 + 9)
+print(69 * 69)
+
+#Mix text and numbers.
+
+print("I'm gonna kms on", 20, "th day.")
+
+""""
+Is this 
+multiline
+comment
+?
+"""
+
+print("Error.")
