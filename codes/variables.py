acf6cda1758629a38da7863da3063f70c659ffa5
MXNTHN <barotmanthan1748@gmail.com>
2026-08-19 17:25:15 +0530

    Basics of Python variables.

--- codes/variables.py
+++ codes/variables.py
@@ -1,6 +1,6 @@
 
-x = 15
-y = "Hello, Nga!"
+x = 19
+y = "Hello, Nga!" #You can use both '' & "" to declare a string. 
 
 print(x)
 print(y)
@@ -9,6 +9,47 @@ print(y)
 
 print("Kill yourself!")
 
-#Using 'print' on the same line.
+a = 18
+a = "Nga"
 
-print("Kill yourself"); print("Kill yourself"); print("Fucking Kill yourself")
+print(a) #Prints string idk y.
+
+b = str(3) #b will be '3' (as a string ig).
+c = int(3) #c will be 3.
+d = float(3) #d will be 3.0
+
+print(b)
+print(c)
+print(d)
+
+#Fetching the types of variables.
+
+print("The type of variable x is",type(x))
+print("The type of variable y is",type(y))
+print("The type of variable a is",type(a))
+print("The type of variable b is",type(b))
+print("The type of variable c is",type(c))
+print("The type of variable d is",type(d))
+
+#Case sensitive
+
+i = 5
+I = "A string"
+
+print(i) #Both are different.
+print(I) #Both are different.
+
+#Assigning multiple values to multiple variables
+
+l, m, n = "This is l", "This is m", "This is n"
+print(l)
+print(m)
+print(n)
+
+#Assigning multiple variables with single value
+
+e = f = g = "Kill yourself"
+
+print(e)
+print(f)
+print(g)
