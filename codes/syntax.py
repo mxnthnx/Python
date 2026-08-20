400fcefc8e497c55d888d2f7e0b5f15116dd85be
MXNTHN <barotmanthan1748@gmail.com>
2026-08-20 23:33:13 +0530

    IDK gng

--- codes/syntax.py
+++ codes/syntax.py
@@ -1,4 +1,3 @@
-
 if 10 < 11:
     print("10 is smaller than 11.")
     
@@ -6,4 +5,12 @@ if 10 < 11:
 
 a = "Nigga"
 print(a[2])
+
 #"2" is the position of the character
+
+x = "next piercing is gonna be a bullet through my skull"
+
+print("bullet" in x)
+
+if "skull" in x:
+    print("Yeah, he's gonna make it all worth it.")
