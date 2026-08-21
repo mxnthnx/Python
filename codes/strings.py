8a42e7e01eecb2bb8e0f7e6fb85e72d5d3d6eb96
MXNTHN <barotmanthan1748@gmail.com>
2026-08-21 22:30:02 +0530

    Playing with strings.

--- codes/strings.py
+++ codes/strings.py
@@ -1,4 +1,5 @@
 x = "next piercing is gonna be a bullet through my skull"
+a = " Blank space before and after quotes "
 
 print("bullet" in x)
 
@@ -9,4 +10,29 @@ if "kill" not in x:
     print("Kill is not in the sentence")
 
 print(x[0:10]) #First character is counted as 0, +1 for empty space between words.
-print(x[28:51])
+print(x[28:51]) #If you use '-', opposite shit...
+
+print(x.upper())
+print(x.lower())
+print(a.strip())
+print(x.replace("n","i"))
+print(a.split(","))
+
+c = x + " " + a
+print(c)
+
+#=================================
+
+fuck = 69
+shit = f"You're lucky, you got {fuck} backshots."
+fucking = f"Nga got {fuck:.3f}" #":.3f" to add decimals
+holy = f"Nga got shot {69*69} times"
+print(shit)
+print(fucking)
+print(holy)
+
+#==========================================
+#Error Code
+#text = "This is a "text"" 
+text = "This is a \"text\"" 
+print(text) #prints <"text">
